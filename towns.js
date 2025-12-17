3d1016269d9372b3357617719108ce8944704ab6
fernjnfv <fernjnfv@gmail.com>
2025-12-18 00:37:36 +0300

    new file:   Barda.pdn 	new file:   Barda_letters.png 	new file:   Orda.pdn 	new file:   Orda_letters.png 	new file:   Orda_map.png 	new file:   Perm_letters.png 	new file:   Perms.pdn 	new file:   barda_map.jpg 	new file:   barda_map.png 	modified:   towns.js

--- towns.js
+++ towns.js
@@ -1,7 +1,9 @@
 Tchaikovsky_town = {region: Tchaikovsky, town_id: "Tchaikovsky", town_name: "Чайковский", town_restorans: [Tchaikovsky_restaurant], map_image: "chaik_map.png", label_image: "Chaik_letters.png"}
-Perm_town = {region: Perm, town_id: "Perm",town_name: "Пермь", town_restorans: [Tchaikovsky_restaurant], map_image: "Perm_map.png", label_image: "Chaik_letters.png"}
+Perm_town = {region: Perm, town_id: "Perm",town_name: "Пермь", town_restorans: [], map_image: "Perm_map.png", label_image: "Perm_letters.png"}
+Barda_town = {region: Barda, town_id: "Barda",town_name: "Барда", town_restorans: [], map_image: "Barda_map.png", label_image: "Barda_letters.png"}
+Orda_town = {region: Orda, town_id: "Orda",town_name: "Орда", town_restorans: [], map_image: "Orda_map.png", label_image: "Orda_letters.png"}
 
-all_regions_town = [Tchaikovsky_town, Perm_town,]
+all_regions_town = [Tchaikovsky_town, Perm_town,Barda_town,Orda_town]
 
 let all_towns = new Map();
 all_regions_town.forEach(town => {
